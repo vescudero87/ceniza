5820c5c39ac6300aaf61cc217b0be0347acd9d8c
vescudero87 <vescudero@igg.unam.mx>
2022-01-11 23:12:19 -0600

    Agregamos print para saber por que tarda

--- process_ash.py
+++ process_ash.py
@@ -32,13 +32,14 @@ b15 = funtions_ceniza.leeNC(b15nc,"CMI")
 dobj = funtions_ceniza.get_time(b04nc)
 
 #Algoritmo
-print ("Aplicando algorítmo")
+print ("Aplicando algorítmo ")
+print ("Calculando transmisividad inversa")
 a = np.subtract(b13, b15)
 b = np.subtract(b11, b13)
 c = np.subtract(b07, b13)
 #d = np.where((a <= 0) & (b >= 0, ) & (c >= 2), 1.0, (a <= 1) & (b >= -0.5) and (c >= 2), 2.0, (a <= 3) and (b >= -1) and (c >= 2) and (b14 < 273), 3.0, 0.0)
 
-
+print ("Aplicando umbrales")
 umbnigth = funtions_ceniza.ceniza_umbral(a,b,c,b14,b04,ds_ref,dobj)
 print ("Algoritmo terminado")
 #Crea tif Geostacionario
